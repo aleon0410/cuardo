90491d889cd02cfedc230e71e632d25b7483ecc5
vmora <vincent.mora@oslandia.com>
2014-07-11 17:34:18 +0200

    parsing multipolygon with tesselation

--- WfsLayer.js
+++ WfsLayer.js
@@ -1,15 +1,17 @@
+// TODO select only the opropriate layer
 WfsLayer = function (url) {
     THREE.Object3D.call( this );
     this.url = url;
-    this.bbox = [];
+    this.extend = [];
     this.srid = 0;
 
     var object = this;
+    cpabilityUrl = url;
     jQuery.ajax(this.url+'?SERVICE=WFS&VERSION=1.1.0&REQUEST=GetCapabilities', {
         success: function(data, textStatus, jqXHR) {
             $(data).find('FeatureType').each(function() { 
                 object.srid =$(this).find('DefaultSRS').text().replace(new RegExp('.*EPSG::'), '');
-                object.bbox = proj4(proj4.defs("EPSG:"+object.srid), $(this).find('ows\\:LowerCorner').text().split(' ')).concat(
+                object.extend = proj4(proj4.defs("EPSG:"+object.srid), $(this).find('ows\\:LowerCorner').text().split(' ')).concat(
                        proj4(proj4.defs("EPSG:"+object.srid), $(this).find('ows\\:UpperCorner').text().split(' ')));
             });
             console.log('loaded');
@@ -22,12 +24,13 @@ WfsLayer = function (url) {
             throw errorThrown;
         }
     });
-    console.log('bbox ' + this.bbox.join(', '));
+    console.log('extend ' + this.extend.join(', '));
 };
 
 WfsLayer.prototype = Object.create( THREE.Object3D.prototype );
 
-WfsLayer.prototype.load = function () {
+WfsLayer.prototype.load = function (extend) {
+    // get the data
 };
 
 
@@ -38,7 +41,7 @@ WfsLayer.prototype.load = function () {
 //	THREE.Object3D.prototype.clone.call( this, object );
 //
 //        object.url  = this.url;
-//        object.bbox = this.bbox;
+//        object.extend = this.extend;
 //        object.srid = this.srid;
 //
 //	return object;
